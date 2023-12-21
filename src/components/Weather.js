2d5aeb7e45b31556e49e0e091cf2c5e70dd9f8d3
NaradaJ <140716249+NaradaJ@users.noreply.github.com>
2023-12-22 04:51:23 +0530

    links connected cards added to About page

---
+++ src/components/Weather.js
@@ -0,0 +1,70 @@
+// Weather.js
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { WiDaySunny, WiCloudy, WiRain, WiSnow, WiThunderstorm } from 'react-icons/wi';
+
+const Weather = ({ apiKey, city }) => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        );
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchWeatherData();
+  }, [apiKey, city]);
+
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  const { main, weather } = weatherData;
+  const temperature = main.temp - 273.15; // Convert temperature from Kelvin to Celsius
+
+  const getWeatherIcon = (iconCode) => {
+    switch (iconCode) {
+      case '01d':
+        return <WiDaySunny />;
+      case '02d':
+        return <WiCloudy />;
+      case '03d':
+      case '04d':
+        return <WiCloudy />;
+      case '09d':
+        return <WiRain />;
+      case '10d':
+        return <WiRain />;
+      case '11d':
+        return <WiThunderstorm />;
+      case '13d':
+        return <WiSnow />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="weather-container">
+      <h2>{city} Weather</h2>
+      <div>
+        <strong>Temperature:</strong> {temperature.toFixed(2)}°C
+      </div>
+      <div>
+        <strong>Weather:</strong> {weather[0].description}
+      </div>
+      <div>
+        {getWeatherIcon(weather[0].icon)}
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
